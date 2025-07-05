a55394830108fd4a8cfbd6eb17e8c61e82202eb1
Tharanika <tharanikabalasubramanian@gmail.com>
2025-07-05 20:46:28 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class package {
+  
+    public package() {
+      
+        Vector v = new Vector();
+
+        java.util.ArrayList l = new java.util.ArrayList();
+    }
+
+    public static void main(String[] args) {
+      
+        new package();
+    }
+}
