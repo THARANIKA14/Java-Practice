a1a1898c39e10fe83156247cbe804f08be34575b
Tharanika <tharanikabalasubramanian@gmail.com>
2025-07-23 21:42:46 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class ArrayList {
+    public static void main (String[] args) {
+        
+          ArrayList<Integer> a = new ArrayList<Integer>();
+          
+          a.add(1);
+          a.add(2);
+          a.add(3);
+      
+          System.out.println(a);
+          
+    }
+}
